fdd05b875545a56a9e5a7c25a67c901394d94c4f
Sid-26 <siddhantdas262@gmail.com>
2024-11-26 16:06:24 -0500

    updated image class to work with undo redo

--- api/image.py
+++ api/image.py
@@ -6,14 +6,52 @@ from base64 import b64decode, b64encode
 class Image:
     def __init__(self, numpy_array, layers=None):
         self.image = numpy_array
-        if not layers:
-            self.layers["layer1"] = self.image
+        self.layers = layers if layers is not None else {"layer1": self.image.copy()}
+        self.layer_count = 1
+        self.current_layer = next(iter(layers))
+        self.undo_stack = []
+        self.redo_stack = []
 
     def __array__(self, dtype=None):
         return self.image.astype(dtype) if dtype else self.image
     
-    def add_layer(self, layer):
-        self.layers.update(layer)
+    def add_layer(self, layer, layer_name=None):
+        self.layer_count += 1
+        if not layer_name:
+            layer_name = f"layer{self.layer_count}"
+        self._save_state()
+        self.layers[layer_name] = layer
+    
+    def change_layer(self, new_layer):
+        raise NotImplementedError
+
+    def remove_layer(self, layer_to_remove):
+        raise NotImplementedError
+    
+    def _save_state(self):
+        state = (self.image.copy(), {k: v.copy() for k, v in self.layers.items()})
+        self.undo_stack(state)
+    
+    def apply_changes(self, new_image):
+        if not isinstance(new_image, np.ndarray):
+            raise ValueError("Input must be a NumPy array.")
+        self._save_state()
+        self.image = new_image
+        self.layers[self.current_layer] = self.image
+        self.redo_stack.clear()
+    
+    def undo(self):
+        if not self.undo_stack:
+            raise ValueError("Nothing to undo")
+        self.redo_stack.append((self.image.copy(), {k: v.copy() for k, v in self.layers.items()}))
+        self.image, self.layers = self.undo_stack.pop()
+    
+    def redo(self):
+        if not self.redo_stack:
+            raise ValueError("Nothing to redo")
+        self.undo_stack.append((self.image.copy(), {k: v.copy() for k, v in self.layers.items()}))
+        
+        self.image, self.layers = self.redo_stack.pop()
 
 def apply_selection(image, mask, function, *args, invert=False, **kwargs):
     mask = mask / 255
