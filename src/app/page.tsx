60fbd3494ac914621c64f7f82f2ed634de5ce0e8
royce-mathew <royce1mathew@gmail.com>
2024-11-26 16:07:48 -0500

    refactor: image processing pipeline

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,8 @@ import clsx from "clsx";
 import { twMerge } from "tailwind-merge";
 import { FileUploader } from "@/lib/custom/file-uploader";
 import Toolset from "@/lib/custom/toolset";
+import { on } from "events";
+
 
 export default function Home() {
   const [currentFile, setCurrentFile] = useState<File[]>([]);
@@ -23,6 +25,65 @@ export default function Home() {
       setCurrentImage(currentFile[0]);
     }
   }, [currentFile]);
+
+
+  function onCommandRan(command: string, base64Image: string, params?: {}) {
+    console.log("Command ran:", command);
+    if (command === "Grayscale") {
+      fetch("http://localhost:5000/grayscale", {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ image: base64Image }),
+      })
+      .then(setImageOnResponse)
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+    }
+  }
+
+  
+  function setImageOnResponse(response: Response) {
+    if (response.ok) {
+      response.json().then((data) => {
+        const base64Image = data.image;
+        const byteCharacters = atob(base64Image);
+        const byteNumbers = new Array(byteCharacters.length);
+        for (let i = 0; i < byteCharacters.length; i++) {
+          byteNumbers[i] = byteCharacters.charCodeAt(i);
+        }
+        const byteArray = new Uint8Array(byteNumbers);
+        const blob = new Blob([byteArray], { type: 'image/png' });
+        const newFile = new File([blob], 'grayscale.png', { type: 'image/png' });
+        setCurrentImage(newFile);
+      }).catch((error) => {
+        console.error("Error parsing JSON response:", error);
+      });
+    } else {
+      console.error("Error in response:", response.statusText);
+    }
+  }
+
+
+  function onButtonPressed(buttonName: string, params?: {}) {
+    if (!currentImage) return;
+    // Convert to base64 image
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      const result = reader.result?.toString();
+      if (result) {
+        const base64Image = result.split(',')[1];
+        if (base64Image) {
+          onCommandRan(buttonName, base64Image, params);
+        }
+      }
+    };
+    // When the file is read, set the current image to the file
+    reader.readAsDataURL(currentImage);
+  }
+
   
 
   // useEffect(() => {
@@ -32,25 +93,27 @@ export default function Home() {
   return (
     <main>
       {/* Canvas */}
-      <div className="flex flex-col mx-10 py-10 px-10">
-        <ImageMenu />
-        <div className="bg-foreground/10 mx-3 py-2 px-2 aspect-square max-h-fit flex flex-row justify-between space-x-3">
-          <Toolset onButtonPressed={(ButtonName) => { console.log(ButtonName); return ButtonName; }} />
-          <div className="flex-1">
-            {currentImage ? (
-              <Image
-                src={URL.createObjectURL(currentImage)}
-                alt="Vercel Logo"
-                width={1080}
-                height={1920}
-                className="w-full h-full object-contain bg-background/20 rounded"
-              />
-            ) : (
-              <FileUploader onValueChange={setCurrentFile}/>
-            )}
+      <div className="flex w-full py-10 px-10 justify-center">
+        <div className="w-[2000px]">
+          <ImageMenu />
+          <div className="bg-foreground/10 mx-3 py-2 px-2 flex flex-row justify-between space-x-3">
+            <Toolset onButtonPressed={onButtonPressed} />
+            <div className="flex-1">
+              {currentImage ? (
+                <Image
+                  src={URL.createObjectURL(currentImage)}
+                  alt="Vercel Logo"
+                  width={1080}
+                  height={1920}
+                  className="w-full max-h-[800px] object-contain py-2 bg-background/20 rounded"
+                />
+              ) : (
+                <FileUploader onValueChange={setCurrentFile}/>
+              )}
+            </div>
           </div>
+          
         </div>
-        
       </div>
     </main>
   );
