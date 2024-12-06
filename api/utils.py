7ca870cf0a3e7d6d5dc2e82bff8d43ec31cd0b59
Sid-26 <siddhantdas262@gmail.com>
2024-12-06 01:36:07 -0500

    refactored and added helper functions

---
+++ api/utils.py
@@ -0,0 +1,30 @@
+from api.index import image_store, HTTPException
+def validate_image(func):
+    """
+    make sure the function is async
+
+    checks if image_store exists (None or not None)
+    """
+    async def wrapper(*args, **kwargs):
+        if image_store is None:
+            raise HTTPException(status_code=400, success=False, detail="No image stored in backend")
+        return await func(*args, **kwargs)
+    
+    return wrapper
+
+def server_exception_handler(status_code=500, detail=""):
+    """
+    default status_code=500
+    
+    default detail="" + Exception as string
+
+    raises HTTPException if something goes wrong
+    """
+    def decorator(func):
+        async def wrapper(*args, **kwargs):
+            try:
+                return await func(*args, **kwargs)
+            except Exception as e:
+                raise HTTPException(status_code=status_code, detail=f"{detail} {str(e)}")
+        return wrapper
+
